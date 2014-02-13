5f84e46289a4b13fb1a4bcf34fe126e05425b5de
Kevin Mann <kevinrmann@gmail.com>
2014-02-13 10:22:46 -0700

    Working on mr kruskels algorithm

--- game-dev/homework2/scripts/renderer.js
+++ game-dev/homework2/scripts/renderer.js
@@ -105,25 +105,155 @@ MYGAME.graphics = (function() {
   }
 
   function Maze() {
-    var size = 10;
+    var mergeGroup = function(group1, group2, maze, size) {
+      for (var row = 0; row < size; row++) {
+        for (var col = 0; col < size; col++) {
+          if (maze[row][col].groupId == group2)
+            console.log(maze[row][col]);
+            console.log('set the groupId equal to the other group id');
+            maze[row][col].groupId == group1;
+            console.log(maze[row][col]);
+        }
+      }
+      return maze;
+    };
+    var size = 5;
     var maze = [];
+    var directions = ['up', 'right', 'down', 'left'];
+    // directions[0] = up
+    // directions[1] = right
+    // directions[2] = down
+    // directions[3] = left
+    var id = 1;
 
+    // Initialize the maze array
+    // false means there is a wall there
     for (var i = 0; i < size; i++) {
       maze.push([]);
-    }
-
+    };
     for (var row = 0; row < size; row++) {
       for (var col = 0; col < size; col++) {
         maze[row].push({
-          'left':   1,
-          'top':    1,
-          'right':  1,
-          'bottom': 1
+          top:     false,
+          right:   false,
+          groupId: id
         });
+        id++;
+      }
+    };
+
+    for (var row = 0; row < size; row++) {
+      for (var col = 0; col < size; col++) {
+        console.log(maze[row][col]);
       }
     }
 
-    // console.log(maze);
+    // Use kruskels algorithm to create the maze
+    // Each time we loop through there will be one less groupId
+    // the maze is done drawing when there is only one group id
+
+    // for (var count = size*size; count > 1; count--) {
+    
+      // Get two random indices for the row and column
+      // and try and get a merge to happen
+      var row = Math.floor(Math.random() * size);
+      var col = Math.floor(Math.random() * size);
+
+      console.log(row);
+      console.log(col);
+      console.log(maze[row][col]);
+      // If not merged lets do a merge to a random adjacent cell
+      // Choose a random direction to merge
+      var i = Math.floor(Math.random() * 4);
+      var merged = false;
+      var availableMergeAttempts = 4;
+      while(!merged) {
+        if (directions[i] == 'up') {
+          console.log('tried to merge up');
+          availableMergeAttempts--;
+
+          // Check to make sure were not merging out of the upper wall
+          if (row==0) {
+            i = 2;
+          } else { // If not, check if a merge is available
+            if (maze[row][col].groupId != maze[row-1][col].groupId) {
+              // Weve got a merge cus the groupId's are different!
+              maze = mergeGroup(maze[row][col].groupId,
+                                maze[row-1][col].groupId,
+                                maze,
+                                size);
+              console.log('merged up');
+              merged = true;
+            } else {
+              i++;
+            }
+          }
+        }
+        if (directions[i] == 'right') {
+          console.log('tried to merge right');
+          availableMergeAttempts--;
+          // Check to make sure were not merging out of the right wall
+          if (col==size-1) {
+            i = 3;
+          } else {
+            if (maze[row][col].groupId != maze[row][col+1].groupId) {
+              // Weve got a merge cus the groupId's are different!
+              maze = mergeGroup(maze[row][col].groupId,
+                                maze[row][col+1].groupId,
+                                maze,
+                                size);
+              console.log('merged right');
+              merged = true;
+            }
+          }
+        }
+        if (directions[i] == 'down') {
+          console.log('tried to merge down');
+          availableMergeAttempts--;
+          if (row==size-1) {
+            i=0
+          } else {
+            if (maze[row][col].groupId != maze[row+1][col].groupId) {
+              // Weve got a merge cus the groupId's are different!
+              maze = mergeGroup(maze[row][col].groupId,
+                                maze[row+1][col].groupId,
+                                maze,
+                                size);
+              console.log('merged up');
+              merged = true;
+            }
+          }
+        }
+        if (directions[i] == 'left') {
+          console.log('tried to merge left');
+          availableMergeAttempts--;
+          if (col==0) {
+            i=1
+          } else {
+            if (maze[row][col].groupId != maze[row+1][col].groupId) {
+              // Weve got a merge cus the groupId's are different!
+              maze = mergeGroup(maze[row][col].groupId,
+                                maze[row][col+1].groupId,
+                                maze,
+                                size);
+              console.log('merged left');
+              merged = true;
+            }
+          }
+        }
+        if (availableMergeAttempts == 0) {
+          console.log('no available merge attempts for this guy... lets get a new vertex to try merging');
+        }
+      }
+
+
+    // }
+
+    for (var row = 0; row < size; row++) {
+      for (var col = 0; col < size; col++) {
+        console.log(maze[row][col]);
+      }
+    }
     
     return maze;
   }
@@ -143,7 +273,9 @@ MYGAME.graphics = (function() {
 //------------------------------------------------------------------
 MYGAME.initialize = (function(graphics) {
 
-  console.log(graphics.Maze());
+
+  var maze = graphics.Maze();
+  // console.log(maze[0][1]);
   
   var myTriangle = graphics.Triangle( {
     center : {x : 150, y : 150 },
